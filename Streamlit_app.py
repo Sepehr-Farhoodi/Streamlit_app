5f4ed84b540e6c13998348429dcfffdacfe29591
Sepehr Farhoodi <sepehrfarhoodi@Sepehrs-Air.lan>
2025-08-23 19:32:19 -0600

    Manually entered lat-long of the landing view

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,30 +5,12 @@ import streamlit as st
 import os
 import requests
 
-#SCRIP_DIR = Path(__file__).parent
-
-
-gdf = gpd.read_file("HYSETS_watershed_boundaries_20200730.shp")
-
-# If CRS is None, set it to NAD83 (common for HYSETS data)
-if gdf.crs is None:
-    gdf.set_crs("EPSG:4269", inplace=True)  # NAD83 geographic
-    print("Set CRS to EPSG:4269 (NAD83)")
-
-# Now transform to WGS84 (EPSG:4326)
-gdf = gdf.to_crs("EPSG:4326")
-
-# Calculate the center of the data
-min_lon, min_lat, max_lon, max_lat = gdf.total_bounds
-center_lat = (min_lat + max_lat) / 2
-center_lon = (min_lon + max_lon) / 2
-
 
 st.set_page_config(layout="wide")
 
 TILES = "https://sepehr-farhoodi.github.io/watershed-tiles/tiles_out/{z}/{x}/{y}.pbf"
 
-view = pdk.ViewState(latitude=center_lat, longitude=center_lon, zoom=4, min_zoom=2, max_zoom=10, bearing=0, pitch=0)
+view = pdk.ViewState(latitude=56.00, longitude=-96.82, zoom=4, min_zoom=2, max_zoom=10, bearing=0, pitch=0)
 
 
 layer = pdk.Layer(
